b780f884c61f893a04efced93b6e85af1f01ae1a
Dr Nilo Stolte <80269251+nilostolte@users.noreply.github.com>
2023-08-30 14:32:42 -0400

    Add files via upload

---
+++ src/ellipses/ellipse6.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+../a -r -p"fill=\"none\" stroke=\"#55aa00\" stroke-width=\"2\"" -ez -m"1 0 0 1 100 100" "M 100 0 A 100 50 0 1 1 100 -1"
+../a -r -p"fill=\"none\" stroke=\"green\" stroke-width=\"2\"" -ez -m".8660254 .5  -0.5 0.8660254 100 100" "M 100 0 A 100 50 0 1 1 100 -1"
+../a -r -p"fill=\"none\" stroke=\"#aa5500\" stroke-width=\"2\"" -ez -m".5 0.8660254 -0.8660254 .5 100 100" "M 100 0 A 100 50 0 1 1 100 -1"
+../a -r -p"fill=\"none\" stroke=\"red\" stroke-width=\"2\"" -ez -m"0 1 -1 0 100 100" "M 100 0 A 100 50 0 1 1 100 -1"
+../a -r -p"fill=\"none\" stroke=\"#aa0055\" stroke-width=\"2\"" -ez -m"-.5 0.8660254 -0.8660254 -.5 100 100" "M 100 0 A 100 50 0 1 1 100 -1"
+../a -r -p"fill=\"none\" stroke=\"blue\" stroke-width=\"2\"" -ez -m"-0.8660254 .5 -.5 -0.8660254 100 100" "M 100 0 A 100 50 0 1 1 100 -1"
